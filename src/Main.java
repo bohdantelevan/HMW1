ab54c08c199637015d6e8de6d628f57832a04cf6
bohdantelevan <televan.bohdan@gmail.com>
2023-06-09 01:33:10 +0300

    hmwrk3

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,133 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Integers: ");
+        Scanner scanner = new Scanner(System.in);
+        int integers[] = new int[scanner.nextInt()];
+        for (int i = 0; i < integers.length; i++) {
 
-        byte one = 1;
-        short two = 2000;
-        int three = 30000;
-        long four = 400000;
-        float five = 5.137f;
-        double six =  6.22;
-        char seven = 'c';
-        boolean eight = true;
+            integers[i] = scanner.nextInt();
+        }
+        System.out.println("Integers contains: ");
+        for (int i = 0; i < integers.length; i++) {
 
-        System.out.println(one + two);//byte + short
+            System.out.println(integers[i]);
+        }
+        System.out.println("Math table");
 
-        System.out.println(three % six);// int % double
+        for (int i = 10; i > 0; i--) {
+            System.out.println();
+            for (int j = 10; j > 0; j--) {
+                System.out.println(i + "*" + j + " = " + i * j);
 
-        System.out.println(eight && four > five); //boolean && long > int
+            }
+        }
+        System.out.println("Whiles");
+        System.out.println();
+        int a = 0;
+        while (a < 4) {
+            System.out.println("Hello" + a);
+            a++;
+        }
+        System.out.println();
+        do {
+            System.out.println("world" + a);
+            a++;
+        } while (a < 7);
 
-        System.out.println(++one);//+1
 
-        System.out.println(seven != six);//not equal
+        System.out.println();
+        System.out.println("2Arrays");
 
-        System.out.println(two << 3);//bitwise
 
-        long far = three;
-
-        System.out.println(far);
-
-        int clear = (int) four;
-
-        System.out.println(clear);
-
-        Plus(one,two);
-
-        Remain(three,six);
-
-        And(two, four);
-
-        PlusOne(two);
-
-        Notequal(seven, six);
-
-        Biwiise(2);
+        int array[][] = new int[scanner.nextInt()][scanner.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            for (int x = 0; x < array[0].length; x++) {
+                array[i][x] = scanner.nextInt();
+            }
+        }
+        System.out.println("Array contains: ");
+        for (int i = 0; i < array.length; i++) {
+            for (int b = 0; b < array[0].length; b++) {
+                System.out.println(array[i][b]);
+            }
         }
 
-    public static void Plus(byte a,short b){
-        System.out.println(a + b);
+        System.out.println();
+        System.out.println("MINIMUM");
+        int min = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+
+                if (array[i][j] < min) {
+                    min = array[i][j];
+                }
+            }
+        }
+        System.out.println("Min = " + min);
+        System.out.println();
+        System.out.println("Quick sort");
+
+        int[] d = {10, -3, 4, 9, 7, 1, -6, 1, 2};
+
+        quicksort(d, 0, d.length - 1);
+
+
+        System.out.println(Arrays.toString(d));
     }
-    public static void Remain(int a,double b){
-        System.out.println(a % b);
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    public static void And(short a,long b){
-        System.out.println(a == b && b < a);
+
+
+    public static int partition(int[] a, int start, int end) {
+        int pivot = a[end];
+
+        int pIndex = start;
+
+
+        for (int i = start; i < end; i++) {
+            if (a[i] <= pivot) {
+                swap(a, i, pIndex);
+                pIndex++;
+            }
+        }
+
+
+        swap(a, end, pIndex);
+
+
+        return pIndex;
     }
-    public static void PlusOne(int a){
-        a++;
-        System.out.println(a);
+
+
+    public static void quicksort(int[] a, int start, int end) {
+
+        if (start >= end) {
+            return;
+        }
+
+
+        int pivot = partition(a, start, end);
+
+
+        quicksort(a, start, pivot - 1);
+
+
+        quicksort(a, pivot + 1, end);
     }
-    public static void Notequal(char a,double b){
-        System.out.println(a != b);
-    }
-    public static void Biwiise(int a){
-        System.out.println(a >> 2);
-    }
-    }
+}
+
+
+
+
+
+
